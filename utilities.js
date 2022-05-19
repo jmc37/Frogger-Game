d09513fe20aad3c6199f9942517b932ea57437d0
Alex Pu <alexander.r.pu@gmail.com>
2022-05-18 19:01:23 -0700

    changed script importing

--- utilities.js
+++ utilities.js
@@ -127,3 +127,28 @@ function scored() {
     frogger.x = canvas.width / 2 - frogger.width / 2;
     frogger.y = canvas.height - frogger.height - 40;
 }
+
+function handleScoreBoard(){
+    ctx4.fillStyle = 'black';
+    ctx4.strokeStyle = 'black';
+    ctx4.font = '15px Verdana';
+    ctx4.strokeText('Score', 265, 15);
+    ctx4.font = '60px Verdana';
+    ctx4.fillText(score, 270, 65);
+    ctx4.font = '15px Verdana';
+    ctx4.strokeText('Collisions: ' + collisionCount, 10, 175);
+    ctx4.strokeText('Game Speed: ' + gameSpeed.toFixed(1), 10, 195);
+}
+
+// collision detection between two rectangles
+function collision(first, second){
+    return !(first.x > second.x + second.width || first.x + first.width < second.x || first.y > second.y + second.height || first.y + first.height < second.y)
+}
+
+function resetGame(){
+    frogger.x = canvas.width/2 - frogger.width/2;
+    frogger.y = canvas.height - frogger.height - 40;
+    score = 0;
+    collisionCount++;
+    gameSpeed = 1;
+}
